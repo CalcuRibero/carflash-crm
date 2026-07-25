087bd17f6026081794b27dd13f231c9e405786c5
Gonzalo Nicolás Ribero Turus <54439472+CalcuRibero@users.noreply.github.com>
2026-07-25 20:45:11 -0300

    Fix manifest problem

--- src/proxy.ts
+++ src/proxy.ts
@@ -51,6 +51,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - public folder
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|public).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|public|manifest.webmanifest).*)",
   ],
 };
