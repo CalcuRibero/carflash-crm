e0513ea651bd9d9cec268d4afb20e4430bfe1652
Gonzalo Nicolás Ribero Turus <54439472+CalcuRibero@users.noreply.github.com>
2026-09-07 16:48:31 -0300

    Fix read notifications

--- src/lib/api/notifications.ts
+++ src/lib/api/notifications.ts
@@ -45,7 +45,6 @@ export async function markNotificationAsRead(id: string): Promise<void> {
   const token = getApiToken() || ""
   const res = await apiRequest(`${API_URL}/notifications/${id}/read`, {
     method: "PATCH",
-    credentials: "include",
     token: token
   });
 
